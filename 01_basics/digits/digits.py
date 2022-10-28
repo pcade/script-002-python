acee2ab946b21a4a569baaf2b5885e02d17da787
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:30:42 +0300

    digit function

--- 01_basics/digits/digits.py
+++ 01_basics/digits/digits.py
@@ -1,8 +1,13 @@
 #!/bin/python3
 
-digit = str(input("Введите число: "))
-finder = 0
-my_list = [int(i) for i in digit]
-for element in range(0, len(my_list)):
-    finder = finder + my_list[element]
-print(f"Сумма цифр равна {finder}")
+
+def digit(any: str) -> int:
+    finder = 0
+    my_list = [int(i) for i in any]
+    for element in range(0, len(my_list)):
+        finder = finder + my_list[element]
+    return(f"Сумма цифр равна {finder}")
+
+any = input("Введите число: ")
+
+print(digit(any))
