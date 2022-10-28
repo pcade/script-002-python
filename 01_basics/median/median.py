51a803ad7815ad84a899ff7e8c04951f65b4796f
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 18:02:08 +0300

    median fun

--- 01_basics/median/median.py
+++ 01_basics/median/median.py
@@ -1,5 +1,16 @@
 #!/bin/python3
 
+def my_list_fun(my_list: list) -> str:
+    if len(my_list) % 2 == 0:
+        sorted_list = sorted(my_list, key= lambda x:float(x))
+        position = int(len(my_list) / 2)
+        result = (sorted_list[position] + sorted_list[position + 1]) / 2
+    else:
+        sorted_list = sorted(my_list, key= lambda x:float(x))
+        position = int(float(len(my_list) / 2 + 0.5))
+        result = sorted_list[position]
+    return(result)
+
 result = float
 my_list = []
 
@@ -9,14 +20,4 @@ while True:
     stop = input("Вы желаете продолжить ввод чисел - Yes or No ? ")
     if stop == 'N' or stop == 'n' or stop == 'NO' or stop == 'no' or stop == 'No' or stop == 'nO':
         break
-
-if len(my_list) % 2 == 0:
-    sorted_list = sorted(my_list, key= lambda x:float(x))
-    position = int(len(my_list) / 2)
-    result = (sorted_list[position] + sorted_list[position + 1]) / 2
-    print(result)
-else:
-    sorted_list = sorted(my_list, key= lambda x:float(x))
-    position = int(float(len(my_list) / 2 + 0.5))
-    result = sorted_list[position]
-    print(result)
+print(my_list_fun(my_list))
