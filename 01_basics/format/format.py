01c97225bb0642bc7d3f659a02b0f1f859aa8450
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:51:53 +0300

    format

--- 01_basics/format/format.py
+++ 01_basics/format/format.py
@@ -11,6 +11,7 @@ def format(name: str, bonus: int, boost: float) -> str:
     final_string = "%."
     print(f"{name} получил премию {bonus} рублей или {boost*100} %.")
     print(name, first_string, bonus, second_string, (boost*100),final_string)
+
 name = 'ВАСЯ'
 bonus = 12345
 boost = 0.12
