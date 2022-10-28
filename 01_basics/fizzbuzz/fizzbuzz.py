2ccdece8e0e31a3b997fdb64106549f11210e18b
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:33:42 +0300

    FizzBuzz function

--- 01_basics/fizzbuzz/fizzbuzz.py
+++ 01_basics/fizzbuzz/fizzbuzz.py
@@ -1,10 +1,13 @@
 #!/bin/python3
 
-for i in range(1, 101):
-    if (i % 5 == 0 and i % 3 == 0):
-        i = 'FizzBuzz'
-    elif i % 5 == 0:
-        i = "Buzz"
-    elif i % 3 == 0: 
-        i = "Fizz"
-    print(i)
+def FizzBuzz():
+    for i in range(1, 101):
+        if (i % 5 == 0 and i % 3 == 0):
+            i = 'FizzBuzz'
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0: 
+            i = "Fizz"
+        print(i)
+
+FizzBuzz()
