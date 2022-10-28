7b2d4bc15de091533c8ad8d2df374457128beb6b
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:55:59 +0300

    hypotenuse funk

--- 01_basics/hypotenuse/hypotenuse.py
+++ 01_basics/hypotenuse/hypotenuse.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
 import math
+
+def hypotenuse(line_1: float, line_2: float) -> str:
+    result = round((math.sqrt(line_1) + math.sqrt(line_2)),2)
+    return(f"Длина гипотенузы {result}")    
+
 line_1 = float(input("Введите длину первого катета: "))
 line_2 = float(input("Введите длину второго катета: "))
-print(f"Длина гипотенузы {round((math.sqrt(line_1) + math.sqrt(line_2)),2)}")
+
+print(hypotenuse(line_1, line_2))
