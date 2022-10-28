add9ca674d8eb5a4d766e4f41b8424d6c99fd2a4
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 18:08:54 +0300

    souvenirs fun

--- 01_basics/souvenirs/souvenirs.py
+++ 01_basics/souvenirs/souvenirs.py
@@ -1,5 +1,9 @@
 #!/bin/python3
 
-S = int(input("Введите количество сувениров: "))
-B = int(input("Введите количество безделушек: "))
-print(f"Общий вес посылки равен {(S*75) + (B*112)} грамм")
+def souvenirs(S: float, B: float) -> str:
+    result = round(((S*75) + (B*112)), 2) # 75 and 112 constant
+    return(f"Общий вес посылки равен {result}")
+
+S = float(input("Введите количество сувениров: "))
+B = float(input("Введите количество безделушек: "))
+print(souvenirs(S, B))
