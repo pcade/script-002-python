6bd1b9026bfd7fdaaa051724671b550813a879ca
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:51:38 +0300

    grade funk

--- 01_basics/grade/grade.py
+++ 01_basics/grade/grade.py
@@ -1,5 +1,14 @@
 #!/bin/python3
 
+
+def grade(digit: int) -> str:
+    my_struct = {digit in range(0, 54) : '2', digit in range(55, 69) : '3', digit in range(70, 84) : '4', digit in range(85, 100) : '5'}
+    print(digit)
+    if digit < 10:
+        result = (f'\n| Баллы | Оценка|\n| ------| ------|\n|   {digit}   |   {my_struct[True]}   |\n')
+    else:
+        result = (f'\n| Баллы  | Оценка |\n| ------ | ------ |\n|   {digit}   |   {my_struct[True]}    |\n')
+    return(result)
+
 digit = int(input('Введите количество баллов: '))
-my_struct = {digit in range(0, 54) : '2', digit in range(55, 69) : '3', digit in range(70, 84) : '4', digit in range(85, 100) : '5'}
-print(f'\n| Баллы  | Оценка |\n| ------ | ------ |\n|   {digit}   |   {my_struct[True]}    |\n')
+print(grade(digit))
