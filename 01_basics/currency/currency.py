bc3d0392776365fcb079cb3c90d3ecc3030bcea0
Григорий Пахомов <gpahomov@astralinux.ru>
2022-10-28 17:25:22 +0300

    функция currency

--- 01_basics/currency/currency.py
+++ 01_basics/currency/currency.py
@@ -1,5 +1,11 @@
 #!/bin/python3
 
+
+def currency(EUR_RUB: float, RUB: float) -> str:
+    CURRENCY = RUB//EUR_RUB
+    return(f"По курсу {EUR_RUB} рублей за 1 евро, на {RUB} рублей вы купите {CURRENCY} евро.")
+
 EUR_RUB = float(input("Введите курс EUR/RUB: "))
 RUB = float(input("Введите количество рублей: "))
-print(f"По курсу {EUR_RUB} на {RUB} рублей вы купите {RUB//EUR_RUB} евро.")
+
+print(currency(EUR_RUB, RUB))
